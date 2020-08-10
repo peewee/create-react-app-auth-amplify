c101e19b2df23bab289c305689bb80f464a76cbb
peewee <pete@comcen.com.au>
2020-08-11 05:38:50 +1000

    balls up

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Svelte
+            Fuck React!
           </a>
         </header>
       </div>
