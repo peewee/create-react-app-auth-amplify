2ba209eddccbe46e8e19643e57e78b144b1052b8
peewee <pete@comcen.com.au>
2020-08-11 16:01:35 +1000

    Hassle

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Fuckb me <code>up the arse</code> and bugger you.
+            Fuck me<code> up the arse</code> and bugger you.
           </p>
           <a
             className="App-link"
