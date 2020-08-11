3a19843598df3fdcc4488720b588e768969ac6e4
peewee <pete@comcen.com.au>
2020-08-11 16:48:28 +1000

    Shit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Svelte
+            Forget React
           </a>
         </header>
       </div>
